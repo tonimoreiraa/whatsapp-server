459aa71344d2d48a080838d50c8240292f0c22b4
Toni Moreira <toni@itentecnologia.com.br>
2023-07-08 12:43:02 -0300

    Send array of messages

--- app/Controllers/Http/MessagesController.ts
+++ app/Controllers/Http/MessagesController.ts
@@ -1,11 +1,13 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Wpp from 'App/Services/Wpp'
 
 export default class MessagesController {
 
-    async store({request}) {
-        const {to, content} = request.only(['to', 'content'])
-        return await Wpp.client.sendMessage(to, content)
+    async store({request}: HttpContextContract) {
+        const messages = await request.input('messages')
+        for (const {to, content} of messages) {
+            await Wpp.client.sendMessage(to, content)
+        }
     }
 
 }
