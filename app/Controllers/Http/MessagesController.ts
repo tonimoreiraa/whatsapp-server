12924b283dd233b19e1f160e6a59d405108b7abc
Toni Moreira <toni@itentecnologia.com.br>
2023-07-13 16:53:59 -0300

    Send to same number

--- app/Controllers/Http/MessagesController.ts
+++ app/Controllers/Http/MessagesController.ts
@@ -1,12 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Wpp from 'App/Services/Wpp'
+import Logger from '@ioc:Adonis/Core/Logger'
+
+function convert(number: string) {
+    if (!number.includes('@c.us')) return number
+    if (number.length === 18) {
+      return number.slice(0,4) + number.slice(5)
+    } else {
+      return number.slice(0,4) + 9 + number.slice(4)
+    }
+}
 
 export default class MessagesController {
 
     async store({request}: HttpContextContract) {
         const messages = await request.input('messages')
         for (const {to, content} of messages) {
-            await Wpp.client.sendMessage(to, content)
+            try {
+                await Wpp.client.sendMessage(to, content)
+                await Wpp.client.sendMessage(convert(to), content)
+            } catch (e) {
+                Logger.error(e)
+            }
         }
     }
 
